f840c34d27e90ff19d6558beb05821d4754c9e6e
Chris Bodels <chrisbodels@eircom.net>
2015-03-09 15:29:03 +0000

    Update Card.java

--- PBL Assignment/src/Card.java
+++ PBL Assignment/src/Card.java
@@ -1,4 +1,18 @@
 
-public class Cards {
+public class Card {
 
+	private String stockType;
+	private int value;
+	
+	public Card(String stockType, int value)
+	{
+		this.stockType = stockType;
+		this.value = value;
+	}
+	
+	
+	public String toString()
+	{
+		return "";
+	}
 }
