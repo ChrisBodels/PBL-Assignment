62a9416b62ebfc3ef2b4845b0ba41a9476f25f39
Paddyk1505 <paddyk1505@gmail.com>
2015-03-09 15:51:24 +0000

    Update Driver.java

--- PBL Assignment/src/Driver.java
+++ PBL Assignment/src/Driver.java
@@ -1,4 +1,39 @@
+import java.util.Scanner;
 
-public class Driver {
-
+public class Driver 
+{
+	public static void main (String args[])
+	{
+		Driver app = new Driver();
+	}
+	
+	private int mainMenu()
+	{ 
+		System.out.println();
+		System.out.println();
+		System.out.println();
+	}
+	
+	private void runMenu()
+	{
+		int option = mainMenu();
+		while (option != 0)
+		{
+			case 1:		;
+			break;
+			case 2:		;
+			break;
+			case 3:		;
+			break;
+			case 4:		;
+			break;
+			case 5:		;
+			break;
+			case 6:		;
+			break;
+			case 7:		;
+			break;
+		}
+		System.out.println("/nPress any key to continue ")
+	}
 }
