cbc855488089be712d602a3631f2d7159f3fd78d
WalshyBoi <20068382@mail.wit.ie>
2015-03-16 16:00:12 +0000

    Update Bank.java

--- PBL Assignment/src/Bank.java
+++ PBL Assignment/src/Bank.java
@@ -12,11 +12,13 @@ public class Bank {
 	private int motorPool, shippingPool, steelPool, storesPool;
 	
 	private ArrayList<Player> players;
+	private ArrayList<Card> cards;
+	Random randomGenerator = new Random();
+	
 	
 	public Bank()
 	{
-		cards = new Card[25];
-		
+	
 		motors = new Stock("Motors");
 		shipping = new Stock("Shipping");
 		steel = new Stock("Steel");
@@ -31,6 +33,7 @@ public class Bank {
 		initialiseCards();
 		
 		players = new ArrayList<Player>();
+		cards =  new ArrayList<Card>();
 	
 	}
 	
@@ -86,36 +89,63 @@ public class Bank {
 	
 	public void initialiseCards()
 	{
-		cards[0] = card0 = new Card("Motors", -4);
-		cards[1] = card1 = new Card("Motors", -3);
-		cards[2] = card2 = new Card("Motors", -2);
-		cards[3] = card3 = new Card("Motors", 2);
-		cards[4] = card4 = new Card("Motors", 3);
-		cards[5] = card5 = new Card("Motors", 4);
+		{
+		card0 = new Card("Motors  ", -4);
+		cards.add(card0);
+		card1 = new Card("Motors  ", -3);
+		cards.add(card1);
+		card2 = new Card("Motors  ", -2);
+		cards.add(card2);
+		card3 = new Card("Motors  ",  2);
+		cards.add(card3);
+		card4 = new Card("Motors  ",  3);
+		cards.add(card4);
+		card5 = new Card("Motors  ",  4);
+		cards.add(card5);
 		
-		cards[6] = card6 = new Card("Shipping", -4);
-		cards[7] = card7 = new Card("Shipping", -3);
-		cards[8] = card8 = new Card("Shipping", -2);
-		cards[9] = card9 = new Card("Shipping", 2);
-		cards[10] = card10 = new Card("Shipping", 3);
-		cards[11] = card11 = new Card("Shipping", 4);
+		card6 = new Card("Shipping", -4);
+		cards.add(card6);	
+	        card7 = new Card("Shipping", -3);
+	        cards.add(card7);
+		card8 = new Card("Shipping", -2);
+		cards.add(card8);
+		card9 = new Card("Shipping",  2);
+		cards.add(card9);
+		card10 = new Card("Shipping", 3);
+		cards.add(card10);
+		card11 = new Card("Shipping", 4);
+		cards.add(card11);
 		
-		cards[12] = card12 = new Card("Steel", -4);
-		cards[13] = card13 = new Card("Steel", -3);
-		cards[14] = card14 = new Card("Steel", -2);
-		cards[15] = card15 = new Card("Steel", 2);
-		cards[16] = card16 = new Card("Steel", 3);
-		cards[17] = card17 = new Card("Steel", 4);
+		card12 = new Card("Steel   ", -4);
+		cards.add(card12);
+		card13 = new Card("Steel   ", -3);
+		cards.add(card13);
+		card14 = new Card("Steel   ", -2);
+		cards.add(card14);
+		card15 = new Card("Steel   ",  2);
+		cards.add(card15);
+		card16 = new Card("Steel   ",  3);
+		cards.add(card16);
+		card17 = new Card("Steel   ",  4);
+		cards.add(card17);
 		
-		cards[18] = card18 = new Card("Stores", -4);
-		cards[19] = card19 = new Card("Stores", -3);
-		cards[20] = card20 = new Card("Stores", -2);
-		cards[21] = card21 = new Card("Stores", 2);
-		cards[22] = card22 = new Card("Stores", 3);
-		cards[23] = card23 = new Card("Stores", 4);
+		card18 = new Card("Stores  ", -4);
+		cards.add(card18);
+		card19 = new Card("Stores  ", -3);
+		cards.add(card19);
+		card20 = new Card("Stores  ", -2);
+		cards.add(card20);
+		card21 = new Card("Stores  ",  2);
+		cards.add(card21);
+		card22 = new Card("Stores  ",  3);
+		cards.add(card22);
+		card23 = new Card("Stores  ",  4);
+		cards.add(card23);
 		
-		cards[25] = card25 = new Card("Bear", -4);
-		cards[24] = card24 = new Card("Bull", 4);
+		card24 = new Card("Bear    ", -4);
+		cards.add(card24);
+		card25 = new Card("Bull    ",  4);
+		cards.add(card25);
 	}
 	
 	public int getStockPrice(String stockType)
@@ -169,4 +199,11 @@ public class Bank {
     		
     		return Str;
     	}
+    	
+    	public String generateCard()
+	{
+	  int index = randomGenerator.nextInt(cards.size());
+	   return cards.get(index).toString();
+	}
+
 }
