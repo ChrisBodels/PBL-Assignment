9946a067ec6632828a95510a0ca7b11aaa99225e
WalshyBoi <20068382@mail.wit.ie>
2015-03-16 16:05:11 +0000

    Update Bank.java

--- PBL Assignment/src/Bank.java
+++ PBL Assignment/src/Bank.java
@@ -3,7 +3,6 @@ import java.util.Random;
 
 public class Bank {
 	
-	private Card[] cards;
 	private Card card0, card1, card2, card3, card4, card5, card6, card7, card8, card9, card10,
 				 card11, card12, card13, card14, card15, card16, card17, card18, card19, card20,
 				 card21, card22, card23, card24, card25;
