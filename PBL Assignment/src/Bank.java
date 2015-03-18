a455f495aa5eac1dd5e8c23f436dca0f38232a67
WalshyBoi <20068382@mail.wit.ie>
2015-03-18 18:14:35 +0000

    Update Bank.java

--- PBL Assignment/src/Bank.java
+++ PBL Assignment/src/Bank.java
@@ -46,60 +46,109 @@ public class Bank {
 		{
 		card0 = new Card("Motors  ", -4);
 		cards.add(card0);
+		removeCards.add(card0);
+		
 		card1 = new Card("Motors  ", -3);
 		cards.add(card1);
+		removeCards.add(card1);
+		
 		card2 = new Card("Motors  ", -2);
 		cards.add(card2);
+		removeCards.add(card2);
+		
 		card3 = new Card("Motors  ",  2);
 		cards.add(card3);
+		removeCards.add(card3);
+		
 		card4 = new Card("Motors  ",  3);
 		cards.add(card4);
+		removeCards.add(card4);
+		
 		card5 = new Card("Motors  ",  4);
 		cards.add(card5);
+		removeCards.add(card5);
 		
 		card6 = new Card("Shipping", -4);
-		cards.add(card6);	
-	        card7 = new Card("Shipping", -3);
-	        cards.add(card7);
+		cards.add(card6);
+		removeCards.add(card6);
+		
+	    card7 = new Card("Shipping", -3);
+	    cards.add(card7);
+	    removeCards.add(card7);
+	    
 		card8 = new Card("Shipping", -2);
 		cards.add(card8);
+		removeCards.add(card8);
+		
 		card9 = new Card("Shipping",  2);
 		cards.add(card9);
+		removeCards.add(card9);
+		
 		card10 = new Card("Shipping", 3);
 		cards.add(card10);
+		removeCards.add(card10);
+		
 		card11 = new Card("Shipping", 4);
 		cards.add(card11);
+		removeCards.add(card11);
 		
 		card12 = new Card("Steel   ", -4);
 		cards.add(card12);
+		removeCards.add(card12);
+		
 		card13 = new Card("Steel   ", -3);
 		cards.add(card13);
+		removeCards.add(card13);
+		
 		card14 = new Card("Steel   ", -2);
 		cards.add(card14);
+		removeCards.add(card14);
+		
 		card15 = new Card("Steel   ",  2);
 		cards.add(card15);
+		removeCards.add(card15);
+		
 		card16 = new Card("Steel   ",  3);
 		cards.add(card16);
+		removeCards.add(card16);
+		
 		card17 = new Card("Steel   ",  4);
 		cards.add(card17);
+		removeCards.add(card17);
+		
 		
 		card18 = new Card("Stores  ", -4);
 		cards.add(card18);
+		removeCards.add(card18);
+		
 		card19 = new Card("Stores  ", -3);
 		cards.add(card19);
+		removeCards.add(card19);
+		
 		card20 = new Card("Stores  ", -2);
 		cards.add(card20);
+		removeCards.add(card20);
+		
 		card21 = new Card("Stores  ",  2);
 		cards.add(card21);
+		removeCards.add(card21);
+		
 		card22 = new Card("Stores  ",  3);
 		cards.add(card22);
+		removeCards.add(card22);
+		
 		card23 = new Card("Stores  ",  4);
 		cards.add(card23);
+		removeCards.add(card23);
 		
 		card24 = new Card("Bear    ", -4);
 		cards.add(card24);
+		removeCards.add(card24);
+		
 		card25 = new Card("Bull    ",  4);
 		cards.add(card25);
+		removeCards.add(card25);
+		
 	}
 	
 	public int getStockPrice(String stockType)
@@ -204,9 +253,9 @@ public class Bank {
     		return Str;
     	}
     	
-    	public int generateCardIndex(Player currentPlayer) {
+    public int generateCardIndex(Player currentPlayer) {//changed
 		int num;
-		int index = randomGenerator.nextInt(cards.size());
+		int index = randomGenerator.nextInt(removeCards.size());
 		num = index;
 		currentPlayer.setCard(num);
 		return index;
