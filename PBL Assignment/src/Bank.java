1f192849e112c3ee3beb9e20ae38a1defde9ef81
WalshyBoi <20068382@mail.wit.ie>
2015-03-23 15:52:57 +0000

    Update Bank.java

--- PBL Assignment/src/Bank.java
+++ PBL Assignment/src/Bank.java
@@ -54,7 +54,6 @@ public class Bank {
 	
 	public void initialiseCards()//UPDATED!!!!
 	{
-		
 		card0 = new Card("Motors",-4);
 		cards.add(card0);
 		removeCards.add(card0);
@@ -62,157 +61,156 @@ public class Bank {
 		removeCards.set(0,card0);
 
 		card1 = new Card("Motors",-3);
-		cards.add(card0);
-		removeCards.add(card0);
+		cards.add(card1);
+		removeCards.add(card1);
 		cards.set(1,card1);
 		removeCards.set(1,card1);
 		
 
 		card2 = new Card("Motors",-2);
-		cards.add(card0);
-		removeCards.add(card0);
+		cards.add(card2);
+		removeCards.add(card2);
 		cards.set(2,card2);
 		removeCards.set(2,card2);
 		
 
 		card3 = new Card("Motors",2);
-		cards.add(card0);
-		removeCards.add(card0);
+		cards.add(card3);
+		removeCards.add(card3);
 		cards.set(3,card3);
 		removeCards.set(3,card3);
 
 		card4 = new Card("Motors",3);
-		cards.add(card0);
-		removeCards.add(card0);
+		cards.add(card4);
+		removeCards.add(card4);
 		cards.set(4,card4);
 		removeCards.set(4,card4);
 
 		card5 = new Card("Motors",4);
-		cards.add(card0);
-		removeCards.add(card0);
+		cards.add(card5);
+		removeCards.add(card5);
 		cards.set(5,card5);
 		removeCards.set(5,card5);
 
 		card6 = new Card("Shipping",-4);
-		cards.add(card0);
-		removeCards.add(card0);
+		cards.add(card6);
+		removeCards.add(card6);
 		cards.set(6,card6);
 		removeCards.add(6,card6);
 
 		card7 = new Card("Shipping",-3);
-		cards.add(card0);
-		removeCards.add(card0);
+		cards.add(card7);
+		removeCards.add(card7);
 		cards.set(7,card7);
 		removeCards.set(7,card7);
 
 		card8 = new Card("Shipping",-2);
-		cards.add(card0);
-		removeCards.add(card0);
+		cards.add(card8);
+		removeCards.add(card8);
 		cards.set(8,card8);
 		removeCards.set(8,card8);
 		
 		card9 = new Card("Shipping",2);
-		cards.add(card0);
-		removeCards.add(card0);
+		cards.add(card9);
+		removeCards.add(card9);
 		cards.set(9,card9);
 		removeCards.set(9,card9);
 
 		card10 = new Card("Shipping",3);
-		cards.add(card0);
-		removeCards.add(card0);
+		cards.add(card10);
+		removeCards.add(card10);
 		cards.set(10,card10);
 		removeCards.set(10,card10);
 
 		card11 = new Card("Shipping",4);
-		cards.add(card0);
-		removeCards.add(card0);
+		cards.add(card11);
+		removeCards.add(card11);
 		cards.set(11,card11);
 		removeCards.set(11,card11);
 
 		card12 = new Card("Steel",-4);
-		cards.add(card0);
-		removeCards.add(card0);
+		cards.add(card12);
+		removeCards.add(card12);
 		cards.set(12,card12);
 		removeCards.set(12,card12);
 
 		card13 = new Card("Steel",-3);
-		cards.add(card0);
-		removeCards.add(card0);
+		cards.add(card13);
+		removeCards.add(card13);
 		cards.set(13,card13);
 		removeCards.set(13,card13);
 
 		card14 = new Card("Steel",-2);
-		cards.add(card0);
-		removeCards.add(card0);
+		cards.add(card14);
+		removeCards.add(card14);
 		cards.set(14,card14);
 		removeCards.set(14,card14);
 
 		card15 = new Card("Steel",2);
-		cards.add(card0);
-		removeCards.add(card0);
+		cards.add(card15);
+		removeCards.add(card15);
 		cards.set(15,card15);
 		removeCards.set(15,card15);
 
 		card16 = new Card("Steel",3);
-		cards.add(card0);
-		removeCards.add(card0);
+		cards.add(card16);
+		removeCards.add(card16);
 		cards.set(16,card16);
 		removeCards.set(16,card16);
 
 		card17 = new Card("Steel",4);
-		cards.add(card0);
-		removeCards.add(card0);
+		cards.add(card17);
+		removeCards.add(card17);
 		cards.set(17,card17);
 		removeCards.set(17,card17);
 
 		card18 = new Card("Stores",-4);
-		cards.add(card0);
-		removeCards.add(card0);
+		cards.add(card18);
+		removeCards.add(card18);
 		cards.set(18,card18);
 		removeCards.set(18,card18);
 
 		card19 = new Card("Stores",-3);
-		cards.add(card0);
-		removeCards.add(card0);
+		cards.add(card19);
+		removeCards.add(card19);
 		cards.set(19,card19);
 		removeCards.set(19,card19);
 
 		card20 = new Card("Stores",-2);
-		cards.add(card0);
-		removeCards.add(card0);
+		cards.add(card20);
+		removeCards.add(card20);
 		cards.set(20,card20);
 		removeCards.set(20,card20);
 
 		card21 = new Card("Stores",2);
-		cards.add(card0);
-		removeCards.add(card0);
+		cards.add(card21);
+		removeCards.add(card21);
 		cards.set(21,card21);
 		removeCards.set(21,card21);
 
 		card22 = new Card("Stores",3);
-		cards.add(card0);
-		removeCards.add(card0);
+		cards.add(card22);
+		removeCards.add(card22);
 		cards.set(22,card22);
 		removeCards.set(22,card22);
 
 		card23 = new Card("Stores",4);
-		cards.add(card0);
-		removeCards.add(card0);
+		cards.add(card23);
+		removeCards.add(card23);
 		cards.set(23,card23);
 		removeCards.set(23,card23);
 
 		card24 = new Card("Bear",-4);
-		cards.add(card0);
-		removeCards.add(card0);
+		cards.add(card24);
+		removeCards.add(card24);
 		cards.set(24,card24);
 		removeCards.set(24,card24);
 
 		card25 = new Card("Bull",4);
-		cards.add(card0);
-		removeCards.add(card0);
+		cards.add(card25);
+		removeCards.add(card25);
 		cards.set(25,card25);
 		removeCards.set(25,card25);
-
 		
 	}
 	
@@ -296,20 +294,7 @@ public class Bank {
 		} else if (stockType == "Stores") {
 			stores.setStockValue(amount);
 		}
-    	
-    	/*
-     * I changed how the stocks are done and this method is now giving errors so I'm just commenting it 
-     * out for now as I can't see it being used anywhere at the moment.
-     * -Chris	
-     */
-    	/*public String checkCurrentBankStock()
-	{String str = "";
-
-		str += ("The current stock in the bank is"+ "\n" + "Motors = " + motorPool + "\n" +"Steel = " + steelPool +
-		"\n"+ "Shipping = " +shippingPool+ "\n" + "Stores = " + storesPool  );
-
-		return str;
-}  */
+    
 	/**
 	 * This method shows the updated value of stocks. 
 	 * @return
@@ -323,29 +308,26 @@ public class Bank {
     		return str;
     	}
     	
-    public void generateCardIndex(Player currentPlayer) {//UPDATED!!
+     public void generateCardIndex() {//UPDATED!!
 
+		for (Player currentPlayer : players){
 		int index = randomGenerator.nextInt(removeCards.size());
+		removeCards.remove(index);
 		currentPlayer.setCard(index);
+		}
+		initialiseCards();
+	}
 
-	}
-	/**
-	 * This method removes the card that has been dealt from the arraylist.
-	 * @param currentPlayer
-	 */
-	public void removeCard(Player currentPlayer) {//UPDATED!!
-	
-		removeCards.remove(currentPlayer.getCard());
-	}
 	
 	public String generateCard(Player currentPlayer) {
-
-		return cards.get(currentPlayer.getCard()).toString();
+		
+        int i = currentPlayer.getCard();
+		return cards.get(i).toString();
 	}
 	
 	public int cardValue(Player currentPlayer){//UPDATED
-		
-		return   cards.get(currentPlayer.getCard()).getValue();
+		int i = currentPlayer.getCard();
+		return   cards.get(i).getValue();
 	}
 	
 	public String getStockType(Player currentPlayer){//UPDATED
